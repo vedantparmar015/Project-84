a3e3987242fb490d3bb251c54744cf8d560a1f43
vedantparmar015 <69168878+vedantparmar015@users.noreply.github.com>
2022-01-16 15:46:46 -0500

    Add files via upload

---
+++ CreatePost.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class CreatePost extends Component {
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Storytelling App</Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={stories}
+              renderItem={this.renderItem}
+            />
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+
+}
+
